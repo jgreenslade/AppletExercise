f755cb3336ee912eee48eed214e773a4b35c8feb
unknown <jgreenslade@belkcom-31.fs.elon.edu>
2015-09-07 16:38:03 -0400

    Fixed text in fields

--- Contact/src/edu/elon/contact/Gui.java
+++ Contact/src/edu/elon/contact/Gui.java
@@ -7,18 +7,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Gui {
-	
+
 	public static final int FRAME_WIDTH = 300;
 	public static final int FRAME_HEIGHT = 250;
-	
+
 	private JFrame frame;
 	private SpringLayout spring;
 	private JMenuBar menuBar;
 	private JMenu m_file, m_edit;
-	private JMenuItem mi_clearDB, mi_connect, mi_exit;
+	private JMenuItem mi_clearDB, mi_connect, mi_exit, mi_add, mi_update, mi_remove;
 	private JTextField tf_fName, tf_mName, tf_lName, tf_eMail, tf_major, tf_userName, tf_password, tf_IPAddress,
 			tf_DBName, tf_TableName;
 	private JButton b_previous, b_next;
+
+	// Arrays
+	// private String[] labels = { "First Name", "Middle Name", "Last Name",
+	// "Email", "Major" };
+	// private JTextField[] textFields = { tf_fName, tf_mName, tf_lName,
+	// tf_eMail, tf_major };
+
+	private String[] labels = { "User Name", "Password", "IPAddress", "Database Name", "Table Name" };
+	private JTextField[] textFields = { tf_userName, tf_password, tf_IPAddress, tf_DBName, tf_TableName };
 
 	/**
 	 * Standard Empty constructor
@@ -33,60 +42,68 @@ public class Gui {
 		spring = new SpringLayout();
 		contentPane.setLayout(spring);
 
-		// Set up menuBar
-		menuBar = new JMenuBar();
+		setUpMenus();
 
-		// Set up menu
-		m_file = new JMenu("Menu");
-
-		// Set up menuItems
-		mi_exit = new JMenuItem("Exit");
-		mi_connect = new JMenuItem("Great");
-
-		m_file.add(mi_exit);
-		m_file.add(mi_connect);
-		
-		
-		// _________________________ Labels and TextFields _________________________
-		
-		String[] labels = { "First Name", "Middle Name", "Last Name", "Email", "Major" };
-		JTextField[] textFields = { tf_fName, tf_mName, tf_lName, tf_eMail, tf_major };
-		
-		// Create and populate the panel.
+		// Create and populate the panel
 		JPanel p = new JPanel(new SpringLayout());
+
 		// Lay out the panel.
-		makePanel(labels, textFields, p);
-		
+		makePanel(p);
+
 		frame.setContentPane(p);
-		//____________________________________________________________________________
-		
+
 		menuBar.add(m_file);
-		// frame.pack();
-		
+		menuBar.add(m_edit);
+
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setVisible(true);
 
 		frame.setJMenuBar(menuBar);
 	}
-	
+
+	private void setUpMenus() {
+		// Set up menuBar
+		menuBar = new JMenuBar();
+
+		// Set up menu
+		m_file = new JMenu("File");
+		m_edit = new JMenu("Edit");
+
+		// Set up menuItems
+		mi_exit = new JMenuItem("Exit");
+		mi_connect = new JMenuItem("Great");
+		mi_clearDB = new JMenuItem("Clear Database");
+		mi_add = new JMenuItem("Add");
+		mi_update = new JMenuItem("Update");
+		mi_remove = new JMenuItem("Remove");
+
+		m_file.add(mi_clearDB);
+		m_file.add(mi_exit);
+		m_file.add(mi_connect);
+		m_edit.add(mi_add);
+		m_edit.add(mi_update);
+		m_edit.add(mi_remove);
+
+	}
+
 	/**
 	 * Get a contact from the contact list based on your current position in it
+	 * 
 	 * @param c
 	 * @param position
 	 */
 	public void getContactInfo(ArrayList<Contact> c, int position) {
 		Contact con = c.get(position);
-		tf_fName.setText(con.getfName());
-		tf_mName.setText(con.getmName());
-		tf_lName.setText(con.getlName());
-		tf_eMail.setText(con.geteMail());
-		tf_major.setText(con.getMajor());
+		textFields[0].setText(con.getfName());
+		textFields[1].setText(con.getmName());
+		textFields[2].setText(con.getlName());
+		textFields[3].setText(con.geteMail());
+		textFields[4].setText(con.getMajor());
 		frame.repaint();
 	}
 
-	private void makePanel(String[] labels, JTextField[] textFields, JPanel p) {
-		// TODO Auto-generated method stub
-		
+	private void makePanel(JPanel p) {
+
 		for (int i = 0; i < labels.length; i++) {
 			JLabel l = new JLabel(labels[i], JLabel.TRAILING);
 			p.add(l);
@@ -94,16 +111,16 @@ public class Gui {
 			l.setLabelFor(textFields[i]);
 			p.add(textFields[i]);
 		}
-		
+
 		b_previous = new JButton("Previous");
 		b_next = new JButton("Next");
-		
-		p.add(Box.createRigidArea(new Dimension(5,5)));
+
+		p.add(Box.createRigidArea(new Dimension(5, 5)));
 		JPanel p2 = new JPanel();
 		p2.add(b_previous);
 		p2.add(b_next);
 		p.add(p2);
-		
+
 		SpringUtilities.makeCompactGrid(p, labels.length + 1, 2, // rows, cols
 				6, 6, // initX, initY
 				6, 6); // xPad, yPad
